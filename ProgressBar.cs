113a0ca1e7cc3dde2b2ae134f4a97bc85b486b18
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-19 13:54:09 +0100

    ProgressBar.cs: Added empty script with TODO.

---
+++ ProgressBar.cs
@@ -0,0 +1,13 @@
+﻿namespace FileToImage.Ui
+{
+    public class ProgressBar
+    {
+        // TODO: 
+        /*
+         * On initialization return amount of progress bar sections allowed by the current window size.
+         *
+         * Have a setter method to set the progress bar one higher.
+         * Could also be a increase() method.
+         */
+    }
+}
