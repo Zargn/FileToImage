75534e59da9923a5474ddb47140d6847e20eac8b
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-19 14:06:19 +0100

    WriteWorker.cs: Added empty class with TODO.

---
+++ WriteWorker.cs
@@ -0,0 +1,11 @@
+﻿namespace FileToImage.Threading
+{
+    public class WriteWorker
+    {
+        // Worker class for writing.
+        /*
+         * Will get a section of the data to convert into bytearrays to be used in creation of the image. One red, green and blue array.
+         * When done will pass the resulting arrays back to the ImageWriter.
+         */
+    }
+}
