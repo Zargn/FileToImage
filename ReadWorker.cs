72a4a1da25d8aaeb28d0321929a35ad110370725
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-19 14:06:07 +0100

    ReadWorker.cs: Added empty class with TODO

---
+++ ReadWorker.cs
@@ -0,0 +1,11 @@
+﻿namespace FileToImage.Threading
+{
+    public class ReadWorker
+    {
+        // Worker class for reading.
+        /*
+         * Will get a section of the rgb arrays from the image.
+         * Then it will combine those arrays into a single long array that is then passed back to the ImageReader.
+         */
+    }
+}
