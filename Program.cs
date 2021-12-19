7ef2945708c7c92558f554dabe716216737d3240
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-19 13:51:18 +0100

    Program.cs: Added TODO list

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace FileToImage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO:
+            /*
+             * Ask used to Create or read a image.
+             * If create, create a ImageCreator instance.
+             * If read, create a ImageReader instance.
+             *
+             * Ask the user for a input file.
+             * If create, it can be any file.
+             * If read, it has to be a png (Or jpg?)
+             *
+             * Pass that file converted to a byte array to the Begin(bytearray) method of the selected class.
+             *
+             * Rest is handled by the classes themselves.
+             */
         }
     }
 }
