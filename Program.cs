2f1a7658ac11b12b011b556f210d257278d42f39
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2021-12-23 22:50:39 +0100

    Program.cs: Started experiments with a custom file selector.
    - Added: FileSelector class.
      Contains:
        Switch statement which takes input in form of up,down,right and left arrow keys.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,71 @@
 ﻿using System;
+using System.IO;
 
 namespace FileToImage
 {
+    public class FileSelector
+    {
+        public void Run()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            MoveBackInDirectory();
+                            break;
+                        case ConsoleKey.RightArrow:
+                            EnterSelection();
+                            break;
+                        case ConsoleKey.UpArrow:
+                            GoUpList();
+                            break;
+                        case ConsoleKey.DownArrow:
+                            GoDownList();
+                            break;
+                        case ConsoleKey.Escape:
+                            Console.WriteLine("EXITING");
+                            return;
+                        case ConsoleKey.Enter:
+                            Console.WriteLine("Confirmed selection");
+                            break;
+                        default:
+                            Console.WriteLine("No valid imput");
+                            break;
+                    }
+                }
+            }
+        }
+        
+        void MoveBackInDirectory()
+        {
+            Console.WriteLine("Moving back!");
+            // TODO: Move back in directory
+        }
+
+        void EnterSelection()
+        {
+            Console.WriteLine("Selected entry");
+            // TODO: Select file/directory
+        }
+
+        void GoUpList()
+        {
+            Console.WriteLine("Went up the list");
+            // TODO: Move up in file list
+        }
+
+        void GoDownList()
+        {
+            Console.WriteLine("Went down the list");
+            // TODO: Move down in file list
+        }
+    }
+    
+    
+    
     class Program
     {
         static void Main(string[] args)
@@ -20,6 +84,9 @@ namespace FileToImage
              *
              * Rest is handled by the classes themselves.
              */
+
+            FileSelector fileSelector = new FileSelector();
+            fileSelector.Run();
         }
     }
     
