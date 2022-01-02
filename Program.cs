6d7151f7ea465cebfce7a97d25d62a2e0074e706
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-02 21:50:27 +0100

    Program.cs: Now has mode selection.

--- Program.cs
+++ Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using FileToImage.Core;
+using FileToImage.Threading;
 
 namespace FileToImage
 {
     class Program
     {
+        private static Mode mode;
+
         static void Main(string[] args)
         {
             // TODO:
@@ -26,13 +30,69 @@ namespace FileToImage
              * Rest is handled by the classes themselves.
              */
 
-            FileSelector fileSelector = new FileSelector(".png");
-            string filePath = fileSelector.GetFileFromUser();
+            mode = Mode.Read;
+            //
+            // FileSelector fileSelector = new FileSelector(".png");
+            // string filePath = fileSelector.GetFileFromUser();
+            //
+            // Console.WriteLine($"Selected file: {filePath}");
 
-            Console.WriteLine($"Selected file: {filePath}");
+            ModeSelect();
+
+            if (mode == Mode.Read)
+            {
+                ReadWorker readWorker = new ReadWorker();
+            }
+            else if (mode == Mode.Write)
+            {
+                WriteWorker writeWorker = new WriteWorker();
+            }
+        }
+
+
+
+        static void ModeSelect()
+        {
+            Console.WriteLine("Please select mode. Cycle options with TAB.");
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.Tab:
+                            CycleMode();
+                            break;
+                        case ConsoleKey.Enter:
+                            Console.WriteLine($"{mode} selected!");
+                            return;
+                    }
+                }
+            }
+        }
+
+        static void CycleMode()
+        {
+            // TODO: Ugly solution, make nicer!
+
+            Console.Clear();
+            if (mode == Mode.Read)
+            {
+                mode = Mode.Write;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(mode);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                mode = Mode.Read;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(mode);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
         }
     }
-    
+
     public enum Mode
     {
         Write,
