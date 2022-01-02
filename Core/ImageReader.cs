f35d86852b199bdd5ecc7c0afe6cd989ffb1ec33
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-01-02 21:58:27 +0100

    ImageReader.cs: Added TODO.

--- Core/ImageReader.cs
+++ Core/ImageReader.cs
@@ -2,6 +2,15 @@
 {
     public class ImageReader
     {
-        
+        public ImageReader()
+        {
+            // Todo:
+            // Ask the user for a png file
+            // Split the file into equal parts for the workers. (Later optimisations could allow smaller chunks that are handed out when a worker is done with previous piece.)
+            // Start the workers and wait for them all to complete and return their data.
+            // Compile the final data into one file.
+            // Ask the user to select a directory to save the file.
+            
+        }
     }
 }
